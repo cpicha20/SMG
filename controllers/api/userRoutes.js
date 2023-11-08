fd5d3f2faa700b5a61a3f924754d18bab061d18a
Roesn <Bubwub04@icloud.com>
2023-11-08 11:20:48 -0500

    review api routes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -25,7 +25,12 @@ router.post('/', async (req, res) => {
 router.post('/login', async (req, res) => {
   try {
     // look for user by username
-    const userData = await User.findOne({ where: { username: req.body.username } });
+    const userData = await User.findOne(
+      {
+        where: {
+          username: req.body.username
+        }
+      });
 
     // if not user found
     if (!userData) {
@@ -50,7 +55,7 @@ router.post('/login', async (req, res) => {
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
-      
+
       res.json({ user: userData, message: 'You are now logged in!' });
     });
 
