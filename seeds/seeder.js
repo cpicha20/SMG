5aa040c70efc4774ce8c578723b2a8f63e734912
Roesn <Bubwub04@icloud.com>
2023-11-08 09:40:59 -0500

    seeder

--- seeds/seeder.js
+++ seeds/seeder.js
@@ -1,6 +1,8 @@
 // import modules
 const gameSeederData = require('./games.js');
-const { Game } = require('../models');
+//const reviewSeederData = require('./reviews.js');
+//const userSeederData = require('./users.js');
+const { Game, Review, User } = require('../models');
 const sequelize = require('../config/connection.js');
 
 // func to seed db with bulkcreate 
@@ -10,6 +12,12 @@ const seedDatabase = async () => {
 
     // bulk create games
     const games = await Game.bulkCreate(gameSeederData, {});
+
+    // bulk create users
+    //const users = await User.bulkCreate(userSeederData, {});
+
+    // bulk create reviews
+    //const reviews = await Review.bulkCreate(reviewSeederData, {});
 
     // exit process after seeding 
     process.exit(0);
