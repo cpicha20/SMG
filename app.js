c8f6ded50665774377ccf6506c0ffc53192dae4d
Roesn <Bubwub04@icloud.com>
2023-11-09 10:34:27 -0500

    changed sync

--- app.js
+++ app.js
@@ -49,6 +49,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 // sync sequelize and db and listen on server 
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
