45522da7d1752958b9469e6aafdfe84d6b088e6e
Roesn <Bubwub04@icloud.com>
2023-11-09 16:48:33 -0500

    router for addign games to collection... still working on it

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,12 +2,14 @@
 const router = require('express').Router();
 
 // import routes
- const userRoutes = require('./userRoutes');
- const reviewRoutes = require('./reviewRoutes');
+const userRoutes = require('./userRoutes');
+const reviewRoutes = require('./reviewRoutes');
+const gameRouter = require('./gameRouter');
 
 // use routes
 router.use('/users', userRoutes);
 router.use('/reviews', reviewRoutes);
+router.use('/games', gameRouter);
 
 // export module
 module.exports = router;
