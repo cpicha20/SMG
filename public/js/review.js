9e7223be82b3b606d3078fcd501c79742771844e
cpicha20 <cpichardowps@gmail.com>
2023-11-09 13:33:10 -0500

    scripting issue

--- public/js/review.js
+++ public/js/review.js
@@ -5,27 +5,26 @@ const postReviewFormHandler = async (event) => {
   // Gather the data from the form elements on the page
   const review_content = document.querySelector('#review-input').value.trim();
   const game_id =  window.location.pathname.split('/').pop();
-
-    
-
   console.log( game_id);
 
   if (review_content) {
     // Send the e-mail and password to the server
-    const response = await fetch('/api/reviews/create', {
+    const response = await fetch('/api/reviews/', {
       method: 'POST',
-      body: JSON.stringify({ review_content,  }),
+      body: JSON.stringify({ review_content, game_id }),
       headers: { 'Content-Type': 'application/json' },
     });
 
     if (response.ok) {
-      document.location.replace('/');
+      console.log("review posted");
+      document.location.reload();
     } else {
-      alert('Failed to log in');
+      alert('Failed to post');
     }
   }
 };
 
+
 document
   .querySelector('#post-review')
-  .addEventListener('submit', postReviewFormHandler);
+  .addEventListener('click', postReviewFormHandler);
